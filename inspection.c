62516e9baeadd17f772875a5bd7a415af443a443
Your Name <you@example.com>
2021-11-10 16:10:49 +0100

    it works, first commit

--- inspection.c
+++ inspection.c
@@ -9,5 +9,46 @@
 #include <string.h>
 
 int main(){
-	sleep(5);
+int fd_from_motor_x, fd_from_motor_z;
+int ret;
+float est_pos_x, est_pos_z;
+fd_set rset;
+struct timeval tv;
+tv.tv_sec = 0;
+tv.tv_usec = 0;
+printf("I am the inspection, these are the current hoist X & Z positions:\n");
+fflush(stdout);
+fd_from_motor_x=open("fifo_est_pos_x", O_RDONLY);
+fd_from_motor_z=open("fifo_est_pos_z", O_RDONLY);
+while(1){
+	FD_ZERO(&rset);
+    FD_SET(fd_from_motor_x, &rset);
+	FD_SET(fd_from_motor_z, &rset);
+    ret = select(FD_SETSIZE, &rset, NULL, NULL, &tv);
+
+    if(ret == -1){ // An error occours.
+        perror("select() on motor x\n");
+    }
+	if (FD_ISSET(fd_from_motor_z, &rset) != 0){
+		
+		read(fd_from_motor_z, &est_pos_z, sizeof(float)); // Update the command.
+
+		
+	}
+	if( FD_ISSET(fd_from_motor_x, &rset) != 0) { // There is something to read!
+	
+		read(fd_from_motor_x, &est_pos_x, sizeof(float)); // Update the command.
+
+	}
+
+
+	// printf("The current estimated X position is: %f ", est_pos_x);
+	// printf("The current estimated Z position is: %f\n", est_pos_z);
+	printf("\rEstimated position (X, Z) = (%.3f,%.3f)", est_pos_x, est_pos_z);
+	fflush(stdout);
+	usleep(500000);
+}
+close(fd_from_motor_x);
+close(fd_from_motor_z);
+return 0;
 }
--- motor_x.c
+++ motor_x.c
@@ -6,14 +6,22 @@
 #include <signal.h>
 #include <sys/time.h>
 
+float float_rand( float min, float max )
+{
+    float scale = rand() / (float) RAND_MAX; /* [0, 1.0] */
+    return min + scale * ( max - min );      /* [min, max] */
+}
+
 float x_position = 0;
+float est_pos_x=0;
+float upper=0.3;
+float lower=-0.3;
 float x_upperbound = 10;
 float x_lowerbound = -10;
 int step = 1;
 
 int main(){
-
-    int fd_x, ret;
+    int fd_x, fd_inspection_x, ret;
     int command = 0;
     fd_set rset;
 
@@ -22,54 +30,64 @@ int main(){
     tv.tv_usec = 0;
 
     printf("I am the motor x\n");
+    fflush(stdout);
+
     fd_x = open("fifo_command_to_mot_x", O_RDONLY);
+    fd_inspection_x=open("fifo_est_pos_x", O_WRONLY);
+
+    printf("dopo open");
+    fflush(stdout);
+
 
     while(1){
         FD_ZERO(&rset);
         FD_SET(fd_x, &rset);
+         fflush(stdout);
         ret = select(FD_SETSIZE, &rset, NULL, NULL, &tv);
 
         if(ret == -1){ // An error occours.
             perror("select() on motor x\n");
         }
 
-        else if( FD_ISSET(fd_x, &rset) > 0 ){ // There is something to read!
+        else if( FD_ISSET(fd_x, &rset) != 0 ){ // There is something to read!
             read(fd_x, &command, sizeof(int)); // Update the command.
         }
         
         if(command == 3){
             //printf("Motor X received: increase\n");
             if (x_position >= x_upperbound){
-                x_position = x_upperbound;
+                
                 command = 6;
-                //printf("Upper limit of the work envelope reached.\n");
+               // printf("Upper X limit of the work envelope reached.\n");
             } else {
                 x_position += step;
             }
-            fflush(stdout);
+
         }
 
         if(command == 4){
-            printf("Motor X received: decrease\n");
+            //printf("Motor X received: decrease\n");
             if (x_position <= x_lowerbound){
                 x_position = x_lowerbound;
                 command = 6;
-                printf("Lower limit of the work envelope reached.\n");
+               // printf("Lower X limit of the work envelope reached.\n");
             } else {
                 x_position -= step;
             }
         }
 
         if(command == 6){
-            printf("Motor X received: STOP\n");
-            fflush(stdout);
         }
 
-        // Sleep for one second, if the command does not change, than repeat again the same command.
-        printf("\rThe current X position is: %.2f", x_position);
+        // Sleep fora a half of one second, if the command does not change, than repeat again the same command.
+        printf("The current X position is: %.2f\n", x_position);
+        fflush(stdout);
+        est_pos_x=x_position+ float_rand(-0.2,0.2); //compute the estimated position
+        write(fd_inspection_x, &est_pos_x, sizeof(float)); //send to inspection konsole
         usleep(500000);
 
     } // End of the while cycle.
 close(fd_x);
+close(fd_inspection_x);
 return 0;
 }

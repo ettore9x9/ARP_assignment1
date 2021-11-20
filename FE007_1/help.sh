bce3cdd6806f5054bafc135995a63c93daa87887
Your Name <you@example.com>
2021-11-20 13:21:42 +0100

    help.sh

---
+++ FE007_1/help.sh
@@ -0,0 +1,7 @@
+more -d ./src/master/master.txt;
+more -d ./src/command/command.txt;
+more -d ./src/motor_x/motor_x.txt;
+more -d ./src/motor_z/motor_z.txt;
+more -d ./src/inspection/inspection.txt;
+more -d ./src/watchdog/watchdog.txt;
+
